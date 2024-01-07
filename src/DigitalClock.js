8b4c0bc030f04922850cf26907ac2360bcf8ecef
ermjonertas <ermert.jonas@yahoo.de>
2024-01-07 15:57:57 +0100

    implemented the clock

---
+++ src/DigitalClock.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+
+function DigitalClock() {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    function formatTime() {
+        let hours = time.getHours();
+        const minutes = time.getMinutes();
+        const seconds = time.getSeconds();
+        const meridiem = hours >= 12 ? 'PM' : 'AM';
+
+        hours = hours % 12 || 12;
+
+        return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`;
+    }
+
+    function padZero(number) {
+        return (number < 10 ? '0' : '') + number;
+    }
+
+    return (
+        <div className="clock-container">
+            <div className="clock">
+                <span>{formatTime()}</span>
+            </div>
+        </div>
+    );
+}
+
+export default DigitalClock;
